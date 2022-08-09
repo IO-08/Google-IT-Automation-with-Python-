3d3382f02794e8d9828c650abf8ee5e214036991
IO <60049721+IO-08@users.noreply.github.com>
2022-08-09 13:53:00 -0700

    Add files via upload

---
+++ returning_values.py
@@ -0,0 +1,9 @@
+def area_triangle (base, height):
+	return base*height/2
+area_a = area_triangle(5,4)
+area_b = area_triangle(7,3)
+sum = area_a +area_b
+print ("The area of the first triangle is: " + str(area_a))
+print ("The area of the second triangle is: " + str(area_b))
+print ("The sum of both areas is: " + str(sum))
+	
